461ae0261858d53964b7d83e9388a78cc83ae2af
Ashish <ashish186.be22@chitkara.edu.in>
2023-03-28 12:32:57 +0530

    final code

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -9,11 +9,31 @@ int main(){
     char choice;
     srand(time(NULL));
 // seed the random number generator
+    do {
+        num = rand() % 100 + 1;
+    // generate a random number between 1 and 100
+        count = 0; 
+        printf("I'm thinking of a number between 1 and 100. Guess what it is ? \n"); 
+        do {
+            printf("Enter your guess : "); 
+            scanf("%d", &guess); 
+            count++; 
+            if (guess > num) { 
+                printf("Too high ! Guess again.\n"); 
+            } else if (guess < num) { 
+                printf("Too low ! Guess again.\n"); 
+            } else { 
+                printf("Congratulations ! You guessed the number in %d attempts .\n", count); 
+            }
+        }while (guess != num);
+        printf("Do you want to play again ? (y/n) ");
+        scanf(" %c", &choice);
+
+// include a space before %c to consume any white space characters in the input buffer 
+        
+        }while (choice == 'y');
+            printf("Thanks for playing ! Goodbye.\n"); 
+
+    return 0; 
+    
 }
-do {
-     num = rand() % 100 + 1;
-// generate a random number between 1 and 100
-     count = 0; 
-     printf("I'm thinking of a number between 1 and 100. Guess what it is!\n");}
-while (choice == 'y');
-     printf("Thanks for playing! Goodbye.\n"); 
